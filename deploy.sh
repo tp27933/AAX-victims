1a2b03e5ac7eae96bc911a7e32bd128e8e3fedbd
Ting Yu Chen <ting@Tings-MacBook-Pro.local>
2023-03-23 22:45:25 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 
 #!/usr/bin/env sh
 # 當發生錯誤時終止腳本運行
-set -e
-# 打包
-npm run build
-# 移動至到打包後的dist目錄 
-cd dist
+# set -e
+# # 打包
+# npm run build
+# # 移動至到打包後的dist目錄 
+# cd dist
 git init
 git add -A
 git commit -m 'deploy'
